0ea4d57f353b0b28c79acdc7a5c9663dba7e1d8c
Sujal Gandhi <sagandhi2005@gmail.com>
2025-04-02 19:50:23 -0400

    Add files via upload

--- pdo_example/books/models/Book.php
+++ pdo_example/books/models/Book.php
@@ -1,27 +1,43 @@
 <?php
+
+require_once '../../config/DBConnection.php';
+
 class Book {
-    private PDO $pdo;
+    private $conn;
 
-    public function __construct(PDO $pdo)
-    {
-        $this->pdo = $pdo;
-        $this->pdo->exec("USE book_db");
+    public function __construct() {
+        $this->conn = (new DBConnection())->connect();
     }
 
-    public function getAll(): ?array
-    {
-        $stm = $this->pdo->query("select * from books");
-        return $stm->fetchAll();
+    public function createBook($title, $author_id) {
+        $query = "INSERT INTO books (title, author_id) VALUES (:title, :author_id)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':author_id', $author_id);
+        return $stmt->execute();
     }
 
-    public function getOneByName(int $id): ?array
-    {
-        $stm = $this->pdo->prepare("select * from books where book_id = :id");
-        $stm->bindValue(":id", $id, PDO::PARAM_INT);
-        $stm->execute();
-
-        return $stm->fetch(PDO::FETCH_ASSOC);
+    public function getBooks() {
+        $query = "SELECT books.id, books.title, authors.name AS author FROM books JOIN authors ON books.author_id = authors.id";
+        $stmt = $this->conn->query($query);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    
+
+    public function updateBook($id, $title, $author_id) {
+        $query = "UPDATE books SET title = :title, author_id = :author_id WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->bindParam(':title', $title);
+        $stmt->bindParam(':author_id', $author_id);
+        return $stmt->execute();
+    }
+
+    public function deleteBook($id) {
+        $query = "DELETE FROM books WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+    }
 }
+
 ?>
